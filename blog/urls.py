320b93d42883b6c3ee2068203142697a5448835d
RaswanthGS <raswanth47@gmail.com>
2022-08-24 11:12:36 +0530

    Update

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path('',views.main, name='main'),
-    path('pgtrb',views.pgtrb, name='pgtrb'),
-    path('polytrb',views.polytrb, name='polytrb'),
-    path('enggtrb',views.enggtrb, name='enggtrb'),
-    path('tnset',views.tnset, name='tnset'),
-    path('ugtrb',views.ugtrb, name='ugtrb'),
+    path('pages/<tag>', views.page),
     path('sample',views.sample, name='sample'),
     path('about',views.about, name='about'),
     path('slogin',views.slogin, name='slogin'),
--- blog/views.py
+++ blog/views.py
@@ -9,42 +9,10 @@ def main(request):
     }
     return render(request, "blog/mainpage.html", context)
 
-def pgtrb(request):
+def page(request, tag):
     context={
-        'contents':Content.objects.filter(nickname='PG TRB'),
-        'posts' : Post.objects.filter(tag='PG TRB').order_by('-date_posted'),
-        'announcements' : Announcement.objects.all()
-    }
-    return render(request, "blog/content.html", context)
-    
-def polytrb(request):
-    context={
-        'contents':Content.objects.filter(nickname='POLY TRB'),
-        'posts' : Post.objects.filter(tag='POLY TRB').order_by('-date_posted'),
-        'announcements' : Announcement.objects.all()
-    }
-    return render(request, "blog/content.html", context)
-
-def enggtrb(request):
-    context={
-        'contents':Content.objects.filter(nickname='ENGG TRB'),
-        'posts' : Post.objects.filter(tag='ENGG TRB').order_by('-date_posted'),
-        'announcements' : Announcement.objects.all()
-    }
-    return render(request, "blog/content.html", context)
-
-def tnset(request):
-    context={
-        'contents':Content.objects.filter(nickname='TNSET'),
-        'posts' : Post.objects.filter(tag='TNSET').order_by('-date_posted'),
-        'announcements' : Announcement.objects.all()
-    }
-    return render(request, "blog/content.html", context)
-
-def ugtrb(request):
-    context={
-        'contents':Content.objects.filter(nickname='UG TRB'),
-        'posts' : Post.objects.filter(tag='UG TRB').order_by('-date_posted'),
+        'contents':Content.objects.filter(nickname=tag),
+        'posts' : Post.objects.filter(nickname=tag).order_by('-date_posted'),
         'announcements' : Announcement.objects.all()
     }
     return render(request, "blog/content.html", context)
